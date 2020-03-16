9106d639c14afbd5b646c1f6ebbcadffb51b4581
autopp <autopp.inc@gmail.com>
2020-03-16 23:22:37 +0900

    feat: impl head

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
+pub enum List {
+    Cons(i64, Box<List>),
+    Nil
+}
+
+pub fn head(l: &List) -> Option<i64> {
+    match l {
+        List::Cons(value, _) => Some(*value),
+        List::Nil => None,
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use super::List::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_head() {
+        assert_eq!(head(&Cons(1, Box::new(Nil))), Some(1));
+        assert_eq!(head(&Nil), None)
     }
 }
