9c069d77df8bd5ca7998a630d86a7e90523e1d6a
autopp <autopp.inc@gmail.com>
2020-03-17 20:24:11 +0900

    feat: impl tail

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Debug,PartialEq)]
 pub enum List {
     Cons(i64, Box<List>),
     Nil
@@ -10,6 +11,13 @@ pub fn head(l: &List) -> Option<i64> {
     }
 }
 
+pub fn tail(l: &List) -> Option<&List> {
+    match l {
+        List::Cons(_, rest) => Some(rest),
+        List::Nil => None
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -19,5 +27,11 @@ mod tests {
     fn test_head() {
         assert_eq!(head(&Cons(1, Box::new(Nil))), Some(1));
         assert_eq!(head(&Nil), None)
+    }
+
+    #[test]
+    fn test_tail() {
+        assert_eq!(tail(&Cons(1, Box::new(Cons(2, Box::new(Nil))))), Some(&Cons(2, Box::new(Nil))));
+        assert_eq!(tail(&Nil), None);
     }
 }
